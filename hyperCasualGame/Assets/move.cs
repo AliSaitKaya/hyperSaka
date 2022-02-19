d4fa68705190878f1b218abde0d5c16e375b3a85
Ali <askayaa@gmail.com>
2022-02-19 15:09:28 +0300

    character move eklendi

---
+++ hyperCasualGame/Assets/move.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class move : MonoBehaviour
+{
+    protected Joystick joystick;
+    float horizontal, vertical = 0;
+    Vector3 FirstPoint;
+    Vector3 SecondPoint;
+    float xAngle;
+    
+    public Animator animator;
+    GameObject wizard;
+    Vector3 forward, right;
+    float moveSpeed = 4f;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        wizard = GameObject.FindWithTag("Wizard");
+        animator = wizard.GetComponent<Animator>();
+
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    private void FixedUpdate()
+    {
+        movement();
+    }
+    // Update is called once per frame
+    void movement()
+    {
+        horizontal = joystick.Horizontal;
+        vertical = joystick.Vertical;
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * horizontal;
+        Vector3 upMovement = forward * moveSpeed *Time.deltaTime * vertical;
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+       Camera.main.transform.position = new Vector3(transform.position.x-10, transform.position.y+7, transform.position.z-8);
+
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+    }
+}
